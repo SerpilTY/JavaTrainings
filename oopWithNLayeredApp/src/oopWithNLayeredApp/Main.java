d1aea404fbe2af53c08c46f4dd582f08447b1764
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-02-24 19:38:53 +0300

    Initial Commit

--- oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,13 +1,14 @@
 package oopWithNLayeredApp;
 
 import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Product product=new Product(1,"IPhone Xr",9);
-        ProductManager productManager=new ProductManager();
+        Product product=new Product(1,"IPhone Xr",10000);
+        ProductManager productManager=new ProductManager(new JdbcProductDao());
         productManager.add(product);
     }
 }
--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,15 +1,30 @@
 package oopWithNLayeredApp.business;
 
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
 public class ProductManager {
+
+    private ProductDao productDao;
+
+    public ProductManager(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
     public void add(Product product) throws Exception {
         //sadece  iş kuralları
         if (product.getUnitPrice()<10){
             throw new Exception("Price can not be less than 10");
         }
-        JdbcProductDao productDao=new JdbcProductDao();
+        //ProductDao productDao=new JdbcProductDao();
+        //
+        //Bir katman başka bir katmanın classını kullanıyorken sadece
+        //interface'inden erişim kurmalıdır.
+        // Entity'ler dışında.. O yüzden 13. satırı kaldırmam gerekiyor.
+        //Bunun yerine interface injection yapıyorum.
+        //9-12 satırlar bağımlılık yönetimi ve son satırda bunu kullandık.
+
         productDao.add(product);
     }
 }
