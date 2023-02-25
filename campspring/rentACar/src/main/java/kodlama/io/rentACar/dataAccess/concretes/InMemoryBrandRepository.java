a4be6c0c381d0e513f9b9d5fa3fe019d7090b849
Serpil81 <44366185+Serpil81@users.noreply.github.com>
2023-02-26 01:19:51 +0300

    Initial Commit

---
+++ campspring/rentACar/src/main/java/kodlama/io/rentACar/dataAccess/concretes/InMemoryBrandRepository.java
@@ -0,0 +1,25 @@
+package kodlama.io.rentACar.dataAccess.concretes;
+
+import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryBrandRepository implements BrandRepository {
+    List<Brand> brands;
+
+    public InMemoryBrandRepository() {
+        brands=new ArrayList<Brand>();
+        brands.add(new Brand(1,"Peugout"));
+        brands.add(new Brand(2,"Volvo"));
+        brands.add(new Brand(3,"Mercedes"));
+        brands.add(new Brand(4,"Opel"));
+        brands.add(new Brand(5,"Audi"));
+        brands.add(new Brand(6,"Mini Cooper"));
+    }
+
+    public List<Brand> getAll() {
+        return brands;
+    }
+}
